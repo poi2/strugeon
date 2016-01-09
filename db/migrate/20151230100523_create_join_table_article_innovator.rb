5171f9b27719abc520a69d38eefc1666cbd73c3e
poi2 <daisuke.ito.cs@gmail.com>
2016-01-09 16:58:48 +0900

    migration file 消し忘れ

--- db/migrate/20151230100523_create_join_table_article_innovator.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableArticleInnovator < ActiveRecord::Migration
-  def change
-    create_join_table :articles, :innovators do |t|
-      t.index [:article_id, :innovator_id]
-      t.index [:innovator_id, :article_id]
-    end
-  end
-end
