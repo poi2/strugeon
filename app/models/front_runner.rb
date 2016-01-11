d07b97a699ecd44f8214790dd2605999bef6aa3f
poi2 <daisuke.ito.cs@gmail.com>
2016-01-11 16:12:48 +0900

    remove bcrypt gem

--- app/models/front_runner.rb
+++ app/models/front_runner.rb
@@ -3,7 +3,6 @@ class FrontRunner < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_secure_password
 
   has_many :deals
 end
